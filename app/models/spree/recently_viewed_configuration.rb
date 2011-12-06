0b2f684b2968d69e7a33257222c1163a529f37ce
John Dyer <lbrapid@gmail.com>
2011-12-06 10:25:27 -0500

    Fixes preference access problems

--- app/models/spree/recently_viewed_configuration.rb
+++ app/models/spree/recently_viewed_configuration.rb
@@ -1,3 +1,3 @@
-class RecentlyViewedConfiguration < Spree::Configuration
+class Spree::RecentlyViewedConfiguration < Spree::Preferences::Configuration
   preference :recently_viewed_products_max_count, :integer, :default => 5
 end
--- lib/spree_recently_viewed.rb
+++ lib/spree_recently_viewed.rb
@@ -1,20 +1,22 @@
 require 'spree_core'
 
+module Spree::RecentlyViewed
+end
+
 module SpreeRecentlyViewed
   class Engine < Rails::Engine
     railtie_name "spree_recently_viewed"
 
     config.autoload_paths += %W(#{config.root}/lib)
 
+    initializer "spree.recently_viewed.preferences", :after => "spree.environment" do |app|
+      Spree::RecentlyViewed::Config = Spree::RecentlyViewedConfiguration.new
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
-
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
-
     end
 
     config.to_prepare &method(:activate).to_proc
