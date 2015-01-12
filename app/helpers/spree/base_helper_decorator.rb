deef66872fb97bbd93153a5348d6342aa8983af3
Tobias Bohwalli <hi@futhr.io>
2015-01-12 06:41:20 +0100

    Prefix helper methods with cache_ instead of get_
    This is more declarative because it fetch data from session.

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,11 +1,11 @@
 module Spree
   BaseHelper.module_eval do
-    def get_recently_viewed_products_ids
+    def cached_recently_viewed_products_ids
       (session['recently_viewed_products'] || '').split(', ')
     end
 
-    def get_recently_viewed_products
-      Spree::Product.find_by_array_of_ids(get_recently_viewed_products_ids)
+    def cached_recently_viewed_products
+      Spree::Product.find_by_array_of_ids(cached_recently_viewed_products_ids)
     end
   end
 end
