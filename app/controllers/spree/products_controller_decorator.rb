8b700eb6deca432639e79ca7d9e6808407f72602
Tobias Bohwalli <hi@futhr.io>
2015-01-26 18:02:16 +0100

    Get preference using preferred_
    Preference defaults to 5 so no need to fallback to this twice.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -7,7 +7,7 @@ module Spree
       rvp = (session['recently_viewed_products'] || '').split(', ')
       rvp.delete(id)
       rvp << id unless rvp.include?(id.to_s)
-      rvp_max_count = Spree::RecentlyViewed::Config[:recently_viewed_products_max_count] || 5
+      rvp_max_count = Spree::RecentlyViewed::Config.preferred_recently_viewed_products_max_count
       rvp.delete_at(0) if rvp.size > rvp_max_count.to_i
       session['recently_viewed_products'] = rvp.join(', ')
     end
