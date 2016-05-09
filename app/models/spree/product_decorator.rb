9d708610cc5888dfa72e29b3ef206ed100e171a0
Vishal Zambre <v.zambre@gmail.com>
2016-05-09 13:11:13 +0530

    removed extra map operation on result
    ```ids.map { |id| products.detect { |product| product.id == id.to_i } }.compact``` is resulting same result of ```products = Spree::Product.where('id IN (?)', ids)```.
    
    Modified query to rails syntax
    No need to assign to new variable products

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,7 @@
 module Spree
   Product.class_eval do
     def self.find_by_array_of_ids(ids)
-      products = Spree::Product.where('id IN (?)', ids)
-      ids.map { |id| products.detect { |product| product.id == id.to_i } }.compact
+      where(id: ids)
     end
   end
 end
