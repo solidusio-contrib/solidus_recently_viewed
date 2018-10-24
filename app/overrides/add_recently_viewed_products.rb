13469031fd24958f6d5a8dcd0107e2b3efe1e119
andrea longhi <andrea@spaghetticode.it>
2018-10-24 15:23:13 +0200

    Add more product page deface hooks
    In order to allow more flexibility, as description is often tweaked on
    customized Solidus apps, a more generic hook `[data-hook='product_show']`
    has been added.

--- app/overrides/add_recently_viewed_products.rb
+++ app/overrides/add_recently_viewed_products.rb
@@ -8,6 +8,6 @@ Deface::Override.new(
 Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'add_recently_viewed_products_to_products_show',
-  insert_after: "#product_description[data-hook], [data-hook='product_description']",
+  insert_after: "#product_description[data-hook], [data-hook='product_description'], [data-hook='product_show']",
   partial: 'spree/shared/add_recently_viewed_products'
 )
