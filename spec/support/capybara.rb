771c91cf5fe9731a4e3bfa3769bffafda2dda087
Angel Perez <iAngel.p93@gmail.com>
2019-02-01 15:23:59 -0400

    Use selenium_chrome_headless as JS driver
    selenium_chrome was invoking a Chrome/Chromium
    instance when tests were run locally. This patch
    uses the headless version, as it should be.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,11 +5,9 @@ require 'capybara-screenshot/rspec'
 RSpec.configure do |config|
   Capybara.save_and_open_page_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
 
-  Capybara.javascript_driver = :selenium_chrome
+  Capybara.javascript_driver = :selenium_chrome_headless
 
   config.before(:each, :js) do
-    if Capybara.javascript_driver == :selenium
-      page.driver.browser.manage.window.maximize
-    end
+    page.driver.browser.manage.window.maximize
   end
 end
