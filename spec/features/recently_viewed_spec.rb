3327fc73e1385ff576c930cd613f844d31be5c14
benjamin wil <benjamin@super.gd>
2024-04-02 12:22:51 -0700

    Improve feature test
    On CI this feature test was flaky. It seems like not all product pages
    were being visited consistently; the errors were about "Jersey" being
    missing from the expected page body text.

--- spec/features/recently_viewed_spec.rb
+++ spec/features/recently_viewed_spec.rb
@@ -5,13 +5,23 @@ RSpec.feature 'Recently Viewed Products', :js do
 
   scenario 'keep track of recently viewed products', js: true do
     visit spree.root_path
-    click_link 'Mug'
-    click_link 'Home'
-    click_link 'Jersey'
-    click_link 'Home'
-    within(:css, 'ul#recently_viewed_products') do
-      expect(page).to have_text 'Mug'
-      expect(page).to have_text 'Jersey'
+
+    click_link "Mug"
+    within "h1.product-title" do
+      expect(page).to have_text "Mug"
+    end
+
+    click_link "Home"
+
+    click_link "Jersey"
+    within "h1.product-title" do
+      expect(page).to have_text "Jersey"
+    end
+
+    click_link "Home"
+    within(:css, "ul#recently_viewed_products") do
+      expect(page).to have_text "Mug"
+      expect(page).to have_text "Jersey"
     end
   end
 end
